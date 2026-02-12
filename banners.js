3227ca1661879c320b614600ff1708fddbcdbafd
ljv6 <maxmohamedmoon@gmail.com>
2026-02-12 05:12:34 +0300

    Create banners.js

---
+++ banners.js
@@ -0,0 +1,16 @@
+const banners = [
+    {
+        title: "دلع جوالك 📲",
+        subtitle: "اشتراكات رقمية رسمية بأسعار خيالية",
+        image: "IMG_7606.jpeg", 
+        opacity: "opacity-80" 
+    },
+    {
+        title: "نجمة السناب",
+        subtitle: "فعل الاشتراك الرسمي خلال دقائق",
+        image: "https://images.unsplash.com/photo-1611162616305-c69b3fa7fbe0?q=80&w=2074",
+        opacity: "opacity-50"
+    }
+];
+
+if (typeof module !== 'undefined') { module.exports = banners; }
