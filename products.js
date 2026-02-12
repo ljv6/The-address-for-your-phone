7e412b4f07f972b5a529dd57aa3bb36a4659092f
ljv6 <maxmohamedmoon@gmail.com>
2026-02-12 05:13:40 +0300

    Create products.js

---
+++ products.js
@@ -0,0 +1,41 @@
+const products = [
+    { 
+        id: 1, 
+        name: "شريحة بيانات STC - 50GB", 
+        price: "80.00", 
+        category: "بيانات وإتصال",
+        img: "IMG_7421.png" 
+    },
+    { 
+        id: 2, 
+        name: "نجمة سناب رسمي (3 أشهر)", 
+        price: "25.00", 
+        category: "سناب شات",
+        img: "IMG_7416.png" 
+    },
+    { 
+        id: 3, 
+        name: "نجمة سناب رسمي (6 أشهر)", 
+        price: "45.00", 
+        category: "سناب شات",
+        img: "IMG_7416.png" 
+    },
+    { 
+        id: 4, 
+        name: "نجمة سناب رسمي (12 شهر)", 
+        price: "90.00", 
+        category: "سناب شات",
+        img: "IMG_7416.png" 
+    },
+    { 
+        id: 5, 
+        name: "ChatGPT Plus (سنة كاملة)", 
+        price: "35.00", 
+        category: "ذكاء اصطناعي",
+        img: "IMG_7422.png" 
+    }
+];
+
+if (typeof module !== 'undefined') {
+    module.exports = products;
+}
